6fe393fb1f7d7819188eba0499de11c77a457919
angeloren92 <angeloren92@gmail.com>
2026-02-04 16:25:32 +0100

    add missing let on counter i

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,7 +11,7 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = []; // in chiesto caso voglio che const sia globale la inizializzo dichiarandola un array
-for (i = teachers.length - 1 ; i >= 0 ; i--) { // abbiamo utilizzato un ciclo forloop, andando a settare
+for (let i = teachers.length - 1 ; i >= 0 ; i--) { // abbiamo utilizzato un ciclo forloop, andando a settare
   //il contatore con la lunghezza dell'array, -1 perchè dalla lunghezza e includendo lo 0 nella condizione, perchè è indice 0,
   //andiamo sottrarre 1 dall'indice fino a quando non è uguale a 0 
   reversedTeachers.push(teachers[i]); // andiamo a manipolare l'array andando ad aggiungere con metodo push gli elementi di teachers
@@ -21,16 +21,18 @@ console.log(reversedTeachers); //loggo il risultato in display
 
 /* Oppure possiamo risolverla contando normalmente, andando ad aggiungere all'inizio gli elementi di teachers in maniera inversa, 
 sfruttando sempre il contatore, in modo che il risultato siano gli elementi reversati
-for (i = 0; i < teachers.length; i++) {
+for (let i = 0; i < teachers.length; i++) {
   reversedTeachers.unshift(teachers[i]);
 }
 console.log(reversedTeachers);
 */
 
-
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let )
+
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
